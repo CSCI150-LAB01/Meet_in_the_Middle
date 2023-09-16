12756e866b334e2c5973923718b14e21e8a6b55b
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2023-09-16 01:15:33 -0700

    add sign up page

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import React from "react";
+import { Input, Button, Link } from "@nextui-org/react";
+import { MdEmail, MdKey } from "react-icons/md";
+
 export default function Login() {
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-between p-24">
-			login
+		<div className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
+			<div className="max-w-xs w-full h-lg flex items-center justify-between font-mono text-sm flex-col gap-4">
+				<p>Sign Up</p>
+
+				<div className="flex-row w-full flex gap-2">
+					<Input type="firstName" label="First Name" />
+					<Input type="lastName" label="Last Name" />
+				</div>
+
+				<Input
+					type="email"
+					label="Email"
+					placeholder="Enter your email"
+					endContent={<MdEmail />}
+				/>
+				<Input
+					type="password"
+					label="Password"
+					placeholder="*********"
+					endContent={<MdKey />}
+				/>
+
+				<Button color="primary" variant="solid" fullWidth>
+					Sign Up
+				</Button>
+				<Button color="primary" variant="solid" fullWidth>
+					Sign Up With Google
+				</Button>
+
+				<Link
+					color="primary"
+					className="w-full text-center"
+					href="#login"
+					size="sm"
+				>
+					Already have an account?
+				</Link>
+			</div>
 		</div>
 	);
 }
