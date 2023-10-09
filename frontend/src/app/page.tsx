c83df4b1ba459a2fea7fdac4e72ade29b8651c70
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2023-10-09 16:53:04 -0700

    add hero

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,38 +1,37 @@
+"use client";
 import { AppConfig } from "@/utils/AppConfig";
 import Image from "next/image";
+import { berlin } from "@/styles/fonts";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex min-h-screen w-full flex-col items-center px-12 lg:px-24 py-12">
+		<div className="flex min-h-screen w-full flex-col items-center px-12 lg:px-24">
 			<main className="flex flex-col max-w-[1000px]">
-				<div className="hero">
-					<h1>{AppConfig.title}</h1>
-					<p>{AppConfig.description}</p>
-				</div>
-				<div className="features" id="features">
-					<h1>Features</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-				</div>
-
-				<div className="Contact" id="Contact">
-					<h1>Contact Us</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
-					</p>
+				<div className="hero bg-base-200 flex">
+					<div className="hero-content flex flex-col lg:flex-row-reverse justify-content items-center py-20">
+						<Image
+							src="/assets/mitm_logo.svg"
+							width={250}
+							height={250}
+							alt="Meet In The Middle Logo"
+							className="w-full hidden sm:block"
+						/>
+						<div className="w-full flex gap-3 flex-col">
+							<h1
+								className={`text-5xl font-bold text-primary ${berlin.className}`}
+							>
+								{AppConfig.title}
+							</h1>
+							<p className="py-6">{AppConfig.description}</p>
+							<Link href="/user/" className="w-fit self-end">
+								<Button color="secondary" variant="ghost" size="lg">
+									Get Started
+								</Button>
+							</Link>
+						</div>
+					</div>
 				</div>
 			</main>
 		</div>
