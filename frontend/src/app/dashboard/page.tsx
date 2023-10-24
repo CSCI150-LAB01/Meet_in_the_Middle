bc207bc0c53dddd77e41c8bb496168e4fc0d7acf
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2023-10-24 12:31:28 -0700

    add the stuff on the figma

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import CardLoading from "@/components/loading";
+import { Card, Skeleton, Button } from "@nextui-org/react";
+
 export default function Dashboard() {
+	const isLoaded = false;
 	return (
 		<>
-			<p>dashboard</p>
+			<p>Location Data</p>
+			<div className="flex flex-col gap-3 w-full p-3">
+				<CardLoading />
+			</div>
+			<p>Upcoming Events</p>
+			<div className="flex flex-col gap-3 w-full p-3">
+				<CardLoading />
+			</div>
+			<p>Friends</p>
+			<div className="flex flex-col gap-3 w-full p-3">
+				<CardLoading />
+			</div>
 		</>
 	);
 }
